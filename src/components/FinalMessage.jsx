35016bcf09f7b6b6388af81cd7ebe36bb954affc
Renishchandera <renishchandera1307@gmail.com>
2025-05-14 22:35:05 +0530

    Updated

--- src/components/FinalMessage.jsx
+++ src/components/FinalMessage.jsx
@@ -14,7 +14,7 @@ export default function FinalMessage()
         description2: "I don’t say this often, but talking to you genuinely makes my day better, Every Single Time.",
         description3: "You’ve still got that same confidence and chaos I remember from 6th class… and honestly, I enjoy it even more now.",
         description4: "From 6th-class silence to Insta nonsense, somehow, you’ve become the best part of my most random days.",
-        description5: "I don’t know how much you miss our chats — but I miss annoying you way more than I should.",
+        description5: "I don’t know how much you miss our chats — but I miss annoying you way more than I should.😅",
         description6: "Once again, Happiest Birthday, Namrata❤️",
         description7: "Always stay as confident, crazy, and amazing as you are.",
     }
